eb1a8097515c3a3acdfde1abbe7f58c952cde8ed
Ryan Mulligan <rmulligan@elevatedthird.com>
2019-06-10 11:59:15 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'Deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hexagoncircle/vue-playlist-filters.git master:gh-pages
+git push -f git@github.com:hexagoncircle/vue-playlist.git master:gh-pages
 
 cd -

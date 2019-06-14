ca55c5eb842ad672a991ebb464c9191d08240613
Ryan Mulligan <rmulligan@elevatedthird.com>
2019-06-14 07:13:29 -0700

    Add loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="loader" aria-busy="true" aria-live="assertive">
+    <span class="loader__bar"></span>
+    <span class="loader__bar"></span>
+    <span class="loader__bar"></span>
+    <span class="loader__bar"></span>
+    <span class="loader__bar"></span>
+    <p class="loader__text">{{title}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Loader',
+
+    props: {
+      title: String
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../styles/components/Loader";
+</style>
